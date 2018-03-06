16102eb6e0e1eff1a129ce6b3ace934bc1ab8dac
MoonLightQ <superqjx@hotmail.com>
2018-03-06 20:57:39 +0800

    Feat ASTExpression s

---
+++ AST/ASTBinaryExpression.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using C.Tokenizer;
+
+namespace C.AST
+{
+    class ASTBinaryExpression : ASTExpression
+    {
+        private Tokenizer.TokenOperator @operator;
+        private ASTExpression rHS;
+        private ASTExpression lHS;
+
+        public TokenOperator Operator { get => @operator; set => @operator = value; }
+        public ASTExpression LHS { get => lHS; set => lHS = value; }
+        public ASTExpression RHS { get => rHS; set => rHS = value; }
+
+        public ASTBinaryExpression(TokenOperator @operator, ASTExpression lHS, ASTExpression rHS)
+        {
+            Operator = @operator;
+            LHS = lHS;
+            RHS = rHS;
+        }
+    }
+}
--- AST/ASTVariableInt.cs
+++ AST/ASTVariableInt.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace C.AST
 {
-    public class ASTInt : ASTExpression
+    class ASTVariableInt : ASTExpression
     {
     }
 }
