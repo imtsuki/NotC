4a230ba3e72e29c5f236844840fd9d9454561e42
imtsuki <me@qjx.app>
2018-10-27 21:38:01 +0800

    Truly Finished!

--- NotC/SyntaxAnalysis/Truth.cs
+++ NotC/SyntaxAnalysis/Truth.cs
@@ -1,12 +1,17 @@
+using NotC.LexicalAnalysis;
 namespace NotC.SyntaxAnalysis {
-    internal class Truth {
-        public static int GetOperatorPrecedence(string @operator) {
+    internal static class Truth {
+        public static int GetOperatorPrecedence(this Token operatorToken) {
+            if (operatorToken.Kind != TokenKind.OPERATOR)
+                return 0;
+            var @operator = (string)operatorToken.Val;
             switch (@operator) {
                 case "*":
                 case "/":
-                    return 1;
+                    return 2;
                 case "+":
                 case "-":
+                    return 1;
                 default:
                     return 0;
             }
